5aedb554262b279530beefa7701308f4585b044f
Kaela Nichols <kae.nichols28@gmail.com>
2019-12-04 21:58:26 -0600

    finally done!

--- main.py
+++ main.py
@@ -1,21 +1,53 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 import html
 import os
 
 app=Flask(__name__)
 app.config['DEBUG'] =True
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://buils-a-blog:pass@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://build-a-blog:pass@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
+db= SQLAlchemy(app)
 
-@app.route('/')
-#routes to newpost.html
-@app.route('/blog')
-#routes to blog.html which displays all blog posts
+class blogpost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    entry = db.Column(db.String(120))
 
-@app.route('/newpost')
+    def __init__(self, title, entry):
+        self.title = title
+        self.entry = entry
 
-blog_title=
-blog_body=
+@app.route('/', methods=['GET'])
+def index():
+    return redirect(url_for('blog'))
 
-app.run()
+
+@app.route('/blog', methods=['GET'])
+def blog():
+    id=request.args.get('id', default=-1)
+    if id == -1:
+        return render_template('blog.html', blogs = blogpost.query.all())
+    else:
+        return render_template('blog.html', singleblog = blogpost.query.get(id))
+
+
+
+@app.route('/newpost', methods=['GET','POST'])
+def newpost():
+    
+    if request.method == 'POST':
+        title = request.form['title']
+        entry = request.form['entry']
+        if title == '' or entry == '':
+            return render_template('newpost.html', title=title, entry=entry)
+        else:
+            blog_post= blogpost(title,entry)
+            db.session.add(blog_post)
+            db.session.commit()
+            return redirect(url_for('blog', id= blog_post.id),code =307)
+    return render_template('newpost.html', title = '', entry='')
+
+
+if __name__ == '__main__':
+    app.run()
