7ce2427785a34b53fe0572ca4e50c215e2831c6f
Kaela Nichols <kae.nichols28@gmail.com>
2019-12-07 17:08:52 -0600

    routing and new html pages

--- main.py
+++ main.py
@@ -12,24 +12,46 @@ db= SQLAlchemy(app)
 class blogpost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
-    entry = db.Column(db.String(120))
+    entry = db.Column(db.String(500))
+    blogger= db.Column(db.Integer)
 
     def __init__(self, title, entry):
         self.title = title
         self.entry = entry
 
 class user(db.Model):
-    userid=
-    email=
-    password=
+    id= db.Column(db.Integer,primary_key=True)
+    username= db.Column(db.String(120))
+    password=db.Column(db.String(120))
+    blogs= db.relationship('blogpost', backref='user.id')
 
     def__init__(self,email, password):
-    self.email=email
+    username = relationship
     self.password=password
 
 @app.route('/', methods=['GET'])
 def index():
     return redirect(url_for('blog'))
+
+@app.route('/signup')
+
+@app.route('/login')
+def login():
+    if request.method=="POST":
+        password = request.form['password']
+        username = request.form['username']
+        email = request.form['email']
+        verify = request.form['verify']
+        ispost = request.method
+        if len(password) > 3 and len(password) < 20 and ' ' not in password:
+            if len(username) > 3 and len(username) < 20 and ' ' not in username:
+                if verify == password and verify != '':
+                    if email == '' or email.count('@') == 1 and email.count('.') == 1 and len(email) > 3 and len(email) < 20 and ' ' not in email:  
+                        return redirect(url_for('welcome'), code=307)
+        return render_template('index.html', username=username,password=password, verify=verify, email=email, ispost=ispost)
+
+
+@all.route('/index')
 
 
 @app.route('/blog', methods=['GET'])
